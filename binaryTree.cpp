085e1254ad091d0acc3b5ce1c25759cea04ef1ed
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-26 15:48:45 +0700

    Melengkapi pilihan menu

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -153,6 +153,8 @@ int main()
         cout << "1. Implement insert operation"<<endl;
         cout << "2. Perform inorder traversal"<<endl;
         cout << "3. Perform preorder traversal"<<endl;
+        cout << "4. Perform postorder traversal"<<endl;
+        cout << "5. Exit"<<endl;
     }
     
 }
