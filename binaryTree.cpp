9acf1ceefc98eed939f351ff94592c38fe79eaf8
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-26 15:17:25 +0700

    Membuat prosedur insert

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -28,7 +28,7 @@ class BinaryTree
 
     void search(int element, Node *&parent, Node *&currentNode)
     {
-        //This fnction seaches the currentNode of the
+        //This function seaches the currentNode of the
         //specified Node as well as the current Node of its parent
         currentNode = ROOT;
         parent = nullptr;
@@ -38,10 +38,28 @@ class BinaryTree
             if (element < currentNode->info)
                 currentNode = currentNode->leftchild;
             else
-            
-            
+                currentNode = currentNode->rightchild;
         }
-        
+    }
 
+    void insert(){
+        int x;
+        cout<< "Masukkan nilai : ";
+        cin >> x;
+
+        //Step 1 : Allocate memory for the new node
+        Node *newNode = new Node();
+
+        //Step 2 : Assign value to the data field of new node
+        newNode->info = x;
+
+        //Step 3 : Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        //Step 4 : Locate the node which will be the parent of the node to be inserted
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode);
     }
 };
